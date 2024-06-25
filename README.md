a91848ba31230e432c6e9366d08a0fb0caae187a
Martin del Pino <119134824+dellpinos@users.noreply.github.com>
2024-06-25 00:31:44 -0300

    seteando datos en el state

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
-
-
-
-
 ## Portfolio 2
-
 
 - cambiar favicon
 - cambiar title
 - agregar control-gastos
 - agregar calculadora nueva
 - trabajar en focus area
+- try - catch en las consultas a la API, de otra forma tengo un #500
+- agregar lazy load o preload a todas las imagenes (especialmente el anillo unico)
+
+
+## Show Entradas
+
+- Problemas al cargar la imagen en los settings "La imagen debe ser un string". Revisar validación y libreria "DropDown".
--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import type { DraftExpense } from "../types";
+import { useState, ChangeEvent } from "react";
+import type { DraftExpense, Value } from "../types";
 import { categories } from "../data/categories";
 import DatePicker from 'react-date-picker';
 import 'react-date-picker/dist/DatePicker.css';
@@ -15,6 +15,24 @@ export default function ExpenseForm() {
         date: new Date()
     });
 
+    const handleChangeDate = (value : Value) => {
+        setExpense({
+            ...expense,
+            date: value
+        });
+    }
+
+    const handleChange = (e : ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+        
+        const { name, value} = e.target;
+        const isAmountField = ['amount'].includes(name);
+
+        setExpense({
+            ...expense,
+            [name] : isAmountField ? +value : value
+        });
+    }
+
     return (
         <form className="space-y-5">
             <legend className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2">Nuevo Gasto</legend>
@@ -28,6 +46,7 @@ export default function ExpenseForm() {
                     placeholder="Añade el nombre del gasto"
                     className="p-2 bg-slate-100"
                     value={expense.expenseName}
+                    onChange={handleChange}
                 />
             </div>
             <div className="flex flex-col gap-2">
@@ -39,11 +58,12 @@ export default function ExpenseForm() {
                     placeholder="Añade la cantidad del gasto: ej. 550"
                     className="p-2 bg-slate-100"
                     value={expense.amount}
+                    onChange={handleChange}
                 />
             </div>
             <div className="flex flex-col gap-2">
                 <label htmlFor="category" className="text-xl">Categoria:</label>
-                <select name="category" id="category" className="p-2 bg-slate-100" value={expense.category}>
+                <select name="category" id="category" className="p-2 bg-slate-100" value={expense.category} onChange={handleChange}>
                     <option value="">-- Seleccione --</option>
                     {categories.map(category => (
                         <option value={category.id} key={category.id}>{category.name}</option>
@@ -56,6 +76,7 @@ export default function ExpenseForm() {
                     <DatePicker
                         className="bg-slate-100 p-2 b-0"
                         value={expense.date}
+                        onChange={ handleChangeDate }
                     />
             </div>
 
